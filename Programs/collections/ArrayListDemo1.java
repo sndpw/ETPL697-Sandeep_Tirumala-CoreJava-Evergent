5fde49227fd1aee0d104a42a7e2d3e1bfcf6f552
Sandeep Tirumala <Sandeep.Tirumala@etplin.com>
2024-08-22 16:59:23 +0530

    added collections

---
+++ Programs/collections/ArrayListDemo1.java
@@ -0,0 +1,15 @@
+package com.evergent.coreJAVA.collections;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo1 {
+
+	public static void main(String[] args) {
+		ArrayList myList = new ArrayList();
+		myList.add(100);
+		myList.add("hello");
+		myList.add(45.5);
+		myList.add(100);
+		System.out.println(myList);
+	}
+}
